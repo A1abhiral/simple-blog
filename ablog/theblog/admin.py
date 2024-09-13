4e4f65237d314375d64caa99039ad465f86f9085
A1abhiral <mr.abhiral.p@gmail.com>
2024-09-13 15:24:34 +0545

    one to one connection between user and Profile

--- ablog/theblog/admin.py
+++ ablog/theblog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from theblog.models import Post,Category
+from theblog.models import Post,Category,Profile
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(Profile)
