546a689de41c61e9e3efc22fa31c7bcba6de49cb
A1abhiral <mr.abhiral.p@gmail.com>
2024-09-25 15:04:32 +0545

    Complete

--- ablog/theblog/forms.py
+++ ablog/theblog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from theblog.models import Post,Category #Comment
+from theblog.models import Post,Category 
 
 choices = Category.objects.all().values_list('name', 'name')
 
@@ -27,21 +27,16 @@ class PostForm(forms.ModelForm):
 class UpdateForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title','body','snippet')
+        fields = ('title','body','header_image','snippet','category')
 
         widgets = {
             'title' : forms.TextInput(attrs={'class':'form-control',}),
             'body' : forms.Textarea(attrs={'class':'form-control',}),
+            'category' : forms.Select(choices=choice_list,attrs={'class':'form-control',}),
             'snippet' : forms.Textarea(attrs={'class':'form-control',}),
+            'header_image': forms.FileInput(attrs={'class': 'form-control'}),
 
 
         }
 
 
-#class CommentForm(forms.ModelForm):
-#    class Meta:
-#        model = Comment
-#        fields = ['body']
-#        widgets = {
- #           'body': forms.Textarea(attrs={'placeholder': 'Write your comment here...', 'class': 'form-control', 'rows': 4}),
-#        }
